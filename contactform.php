02a6723653d410787639d836868d234014ea7434
eneroh <28052446+Eneroh@users.noreply.github.com>
2021-12-31 03:37:18 +1100

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php>
+  if(isset($_POST['submit']))  {
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
+    $country = $_POST['country'];
+    $mailFrom = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $mailTo = "pinoy112@gmail.com";
+    $headers = "From: " .$mailfrom;
+    $txt = "You have received an e-mail from " .$fname $lname from $country.".\n\n".$message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.php?mailsend");
+  }
+?>
